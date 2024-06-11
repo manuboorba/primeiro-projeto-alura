a499acc8a19f2a519832f9a1c21314171d63b79f
Manuela <manuelacavalcanti3@gmail.com>
2024-06-11 17:23:16 -0300

    Primeiro commit

---
+++ src/PrimeiraClasse.java
@@ -0,0 +1,12 @@
+public class PrimeiraClasse {
+    public static void main(String[] args) {
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano do lançamento: " + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+
+    }
+}
