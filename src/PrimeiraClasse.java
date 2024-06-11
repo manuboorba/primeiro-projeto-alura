e459150c3bcde265abca8b58b5b21d6bef4d35bf
Manuela <manuelacavalcanti3@gmail.com>
2024-06-11 19:03:15 -0300

    atualizando

--- src/PrimeiraClasse.java
+++ src/PrimeiraClasse.java
@@ -8,5 +8,20 @@ public class PrimeiraClasse {
         boolean incluidoNoPlano = true;
         double notaDoFilme = 8.1;
 
+        double notaDoFilmeJaqueline = 9.8;
+        double notaDoFilmePaulo = 6.1;
+        double notaDoFilmeManu = 7.0;
+        double media = (notaDoFilmeJaqueline + notaDoFilmePaulo + notaDoFilmeManu) /3;
+        System.out.println(media);
+        String sinopse = """
+                Filme Top Gun
+                Filme de aventura com galã dos anos 80.
+                Ano de lançamento:
+                """ + anoDeLancamento;
+        System.out.println(sinopse);
+
+        int classificacao = (int) (media/2);
+        System.out.println(classificacao);
+
     }
 }
